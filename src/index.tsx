a1233adfc23ec29f83140f290c27b8542c8e6ed3
su-san1129 <deviationvalue1023@outlook.jp>
2020-07-04 00:13:46 +0900

    チュートリアル完成！

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { type } from "os";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+type SquareType = string | null;
+
+type SquareProps = {
+  value: SquareType;
+  onClick: () => void;
+};
+
+type BoardState = {
+  squares: SquareType[];
+  xIsNext: boolean;
+};
+
+const BoardValue = {
+  PLAYER1: "X",
+  PLAYER2: "O",
+};
+
+// ます目を表すクラス
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
-class Board extends React.Component {
+// ゲーム全体を表示するクラス
+class Board extends React.Component<any, BoardState> {
   renderSquare(i: number) {
-    return <Square />;
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -42,26 +62,113 @@ class Board extends React.Component {
     );
   }
 }
+type HistoryData = {
+  squares: SquareType[];
+};
 
-class Game extends React.Component {
+type GameState = {
+  history: HistoryData[];
+  xIsNext: boolean;
+  stepNumber: number;
+};
+
+class Game extends React.Component<any, GameState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array<SquareType>(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+      stepNumber: 0,
+    };
+  }
+
+  handleClick(i: number): void {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? BoardValue.PLAYER1 : BoardValue.PLAYER2;
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    const status = winner
+      ? `Winner: ${winner}`
+      : `Next player: ${
+          this.state.xIsNext ? BoardValue.PLAYER1 : BoardValue.PLAYER2
+        }`;
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i: number) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
 
+function calculateWinner(squares: SquareType[]): SquareType {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
